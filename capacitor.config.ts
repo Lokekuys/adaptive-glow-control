06c9701ccc831b1920695ed434b1fc75edaa7c11
Luke <carcedoluke2003@gmail.com>
2026-02-06 18:45:41 +0800

    Android added via Capacitorjs

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'VCCion',
+  webDir: 'dist'
+};
+
+export default config;
